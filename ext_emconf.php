0e4ead204d25fbb17c63199241e90e9daec36d60
Tim Lochmüller <Spyker@Tims-MacBook-Pro.local>
2015-07-17 09:10:38 +0200

    Add dependency to css_styled_content

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,8 +18,9 @@ $EM_CONF['ink'] = array(
 	'author_email'     => 'tim@fruit-lab.de',
 	'constraints'      => array(
 		'depends' => array(
-			'typo3'      => '6.2.0-7.99.99',
-			'autoloader' => '1.6.0-0.0.0',
+			'typo3'              => '6.2.0-7.99.99',
+			'css_styled_content' => '6.2.0-7.99.99',
+			'autoloader'         => '1.6.0-0.0.0',
 		),
 	),
 );
