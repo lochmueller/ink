8150c39979e33b48956095b83f850120be349917
Tim Lochmüller <Spyker@Tims-MacBook-Pro.local>
2015-07-18 10:02:40 +0200

    Migratoe more classes

--- Classes/PlainRenderer.php
+++ Classes/PlainRenderer.php
@@ -7,10 +7,9 @@
 namespace FRUIT\Ink;
 
 use FRUIT\Ink\Rendering\RenderingInterface;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\MailUtility;
-use TYPO3\CMS\Extbase\Reflection\MethodReflection;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 
 /**
  *  PlainText Service
@@ -32,79 +31,6 @@ class PlainRenderer {
 	protected $conf;
 
 	/**
-	 * Registerd cTypes
-	 *
-	 * @var array
-	 */
-	protected $registeredCTypes = array();
-
-	/**
-	 * Build up the object
-	 */
-	function __construct() {
-		$this->registerCType();
-	}
-
-	/**
-	 * Register a CType
-	 */
-	protected function registerCType() {
-		/** @var $classReflection \TYPO3\CMS\Extbase\Reflection\ClassReflection */
-		$classReflection = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Reflection\\ClassReflection', get_class($this));
-		$aMethods = $classReflection->getMethods();
-
-		foreach ($aMethods as $method) {
-			/** @var MethodReflection $method */
-			if ($method->isTaggedWith('CType')) {
-				$cType = implode('', $method->getTagValues('CType'));
-				$methodName = $method->getName();
-				$this->registeredCTypes[$cType] = $methodName;
-			}
-		}
-	}
-
-	/**
-	 * Check if a cType is registered
-	 *
-	 * @param string $cType
-	 *
-	 * @return boolean
-	 */
-	public function isRegistered($cType) {
-		return isset($this->registeredCTypes[$cType]);
-	}
-
-	/**
-	 * Function used to wrap the bodytext field content (or image caption) into lines of a max length of
-	 *
-	 * @param        string $str : The content to break
-	 *
-	 * @return        string                Processed value.
-	 * @see main_plaintext(), breakLines()
-	 */
-	function breakContent($str) {
-		$cParts = explode(chr(10), $str);
-		$lines = array();
-		foreach ($cParts as $substrs) {
-			$lines[] = $this->breakLines($substrs, LF);
-		}
-		return implode(chr(10), $lines);
-	}
-
-	/**
-	 * Breaking lines into fixed length lines, using GeneralUtility::breakLinesForEmail()
-	 *
-	 * @param        string  $str       : The string to break
-	 * @param        string  $implChar  : Line break character
-	 * @param        integer $charWidth : Length of lines, default is $this->charWidth
-	 *
-	 * @return        string                Processed string
-	 */
-	function breakLines($str, $implChar = LF, $charWidth = 76) {
-		return MailUtility::breakLinesForEmail($str, $implChar, Configuration::getPlainTextWith());
-	}
-
-	/**
 	 * Render the different elements and collect the single lines.
 	 * After the rendering the lines will be imploded. Notice:
 	 * All methods after this are CType rendering helper
@@ -123,14 +49,22 @@ class PlainRenderer {
 		}
 
 		$renderer = array(
-			'html'    => 'FRUIT\\Ink\\Rendering\\Html',
-			'header'  => 'FRUIT\\Ink\\Rendering\\Header',
-			'table'   => 'FRUIT\\Ink\\Rendering\\Table',
-			#'menu'   => 'FRUIT\\Ink\\Rendering\\Menu',
-			'text'    => 'FRUIT\\Ink\\Rendering\\Text',
-			'image'   => 'FRUIT\\Ink\\Rendering\\Image',
-			'textpic' => 'FRUIT\\Ink\\Rendering\\TextPicture',
+			'html'            => 'FRUIT\\Ink\\Rendering\\Html',
+			'header'          => 'FRUIT\\Ink\\Rendering\\Header',
+			'table'           => 'FRUIT\\Ink\\Rendering\\Table',
+			#'menu'            => 'FRUIT\\Ink\\Rendering\\Menu',
+			'text'            => 'FRUIT\\Ink\\Rendering\\Text',
+			#'image'           => 'FRUIT\\Ink\\Rendering\\Image',
+			'textpic'         => 'FRUIT\\Ink\\Rendering\\TextPicture',
+			'templavoila_pi1' => 'FRUIT\\Ink\\Rendering\\Templavoila',
+			'list'            => 'FRUIT\\Ink\\Rendering\\Plugin',
 		);
+
+		$objectManager = new ObjectManager();
+		/** @var Dispatcher $signalSlot */
+		$signalSlot = $objectManager->get('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+		$returnData = $signalSlot->dispatch(__CLASS__, 'renderer', array('renderer' => $renderer));
+		$renderer = $returnData['renderer'];
 
 		if (isset($renderer[$CType])) {
 			$className = $renderer[$CType];
@@ -138,87 +72,10 @@ class PlainRenderer {
 			$renderObject = GeneralUtility::makeInstance($className);
 			$lines = $renderObject->render($this->cObj, $this->conf);
 		} else {
-
-			if ($this->isRegistered($CType)) {
-				$func = $this->registeredCTypes[$CType];
-				$lines = $this->$func($lines);
-			} else {
-				$lines[] = 'CType: ' . $CType . ' have no rendering definitions';
-			}
+			$lines[] = 'CType: ' . $CType . ' have no rendering definitions';
 		}
 		$content = implode(LF, $lines);
 		return trim($content, CRLF . TAB);
-	}
-
-	/**
-	 * Render TV FCE
-	 *
-	 * @CType templavoila_pi1
-	 *
-	 * @param array $lines
-	 *
-	 * @return array
-	 */
-	public function renderTemplavoilaPi1($lines = array()) {
-		// TV nicht geladen
-		if (!ExtensionManagementUtility::isLoaded('templavoila')) {
-			$defaultOutput = $this->conf['defaultOutput'];
-			if ($defaultOutput) {
-				$defaultOutput = str_replace('###CType###', $this->cObj->data['CType'], $defaultOutput);
-			}
-			return $defaultOutput;
-		}
-		$pi1 = GeneralUtility::makeInstance('tx_templavoila_pi1');
-		$pi1->cObj = $this->cObj;
-		$lines[] = $pi1->renderElement($this->cObj->data, 'tt_content');
-		return $lines;
-	}
-
-	/**
-	 * Experimental rendering for plugins:
-	 * Based on sg_plaintext from Stefan Geith
-	 *
-	 * @CType list
-	 *
-	 * @param array $lines
-	 *
-	 * @return array
-	 */
-	public function renderList($lines = array()) {
-		$myName = FALSE;
-		$listType = $this->cObj->data['list_type'];
-		$template = $GLOBALS['TSFE']->tmpl;
-		if (isset($template->setup['tt_content.']['list.']['20.'][$listType])) {
-			$theValue = $template->setup['tt_content.']['list.']['20.'][$listType];
-			$theConf = $template->setup['tt_content.']['list.']['20.'][$listType . '.'];
-		} else {
-			$tmp = explode('_pi', $listType);
-			if (count($tmp) < 2) {
-				$myName = 'tx_' . str_replace('_', '', $tmp[0]);
-			} else {
-				$myName = 'tx_' . str_replace('_', '', $tmp[0]) . '_pi' . $tmp[1];
-			}
-			$theValue = $template->setup['plugin.'][$myName];
-			$theConf = $template->setup['plugin.'][$myName . '.'];
-		}
-		$content = $this->cObj->cObjGetSingle($theValue, $theConf);
-
-		$myContent = $this->breakContent(strip_tags($content));
-		if (strlen($myContent) > 1) {
-			if (substr($myContent, 0, 1) == '|' && substr($myContent, -1) == '|') {
-				$myContent = substr($myContent, 1, strlen($myContent) - 2);
-			}
-		}
-
-		if (!is_array($theConf) || strlen($theConf['plainType']) < 2) {
-			$defaultOutput = $this->conf['defaultOutput'];
-			if ($defaultOutput && $myName) {
-				$lines[] = str_replace('###CType###', $this->cObj->data['CType'] . ': "' . $this->cObj->data['list_type'] . '"; plugin: "' . $myName . '"; plainType: "' . (is_array($theConf) ? $theConf['plainType'] : '') . '"', $defaultOutput);
-			}
-		}
-
-		$lines[] = $myContent;
-		return $lines;
 	}
 
 	/**
