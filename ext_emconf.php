dbb5c6ae70f8c7a654d44ebc42aeb7c04f3a9d44
Tim Lochmüller <tim@fruit-lab.de>
2020-02-29 14:35:35 +0100

    Fix dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,9 +18,9 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email'     => 'tim@fruit-lab.de',
     'constraints'      => array(
         'depends' => array(
-            'typo3'              => '6.2.0-7.99.99',
-            'css_styled_content' => '6.2.0-7.99.99',
-            'autoloader'         => '1.6.0-0.0.0',
+            'typo3'              => '7.6.0-8.5.99',
+            'css_styled_content' => '7.6.0-8.5.99',
+            'autoloader'         => '3.0.0-0.0.0',
         ),
     ),
 );
